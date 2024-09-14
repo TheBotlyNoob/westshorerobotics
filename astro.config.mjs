2d1aff2371676469f38dfc3a1e8d360c60c3e0de
Jay <thebotlynoob@gmail.com>
2024-09-14 00:23:44 +0000

    wip

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), icon()],
+  site: "https://jjay.dev",
+  base: "ws-robotics",
+});
