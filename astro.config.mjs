8e1e30e3fb8640553e8102a4bea31e35deae2fda
Jay <thebotlynoob@gmail.com>
2024-09-20 12:00:21 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import icon from "astro-icon";
 export default defineConfig({
   integrations: [tailwind(), icon()],
   site: "https://jjay.dev",
-  base: "ws-ftc",
+  base: "westshorerobotics",
 });
